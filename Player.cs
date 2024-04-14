ea4098204cea4190d8fe2fc1819e8d243392188b
Geeste <157949884+Geeste@users.noreply.github.com>
2024-04-14 11:36:09 +0300

    Add files via upload

--- Player.cs
+++ Player.cs
@@ -4,11 +4,45 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private int health = 10;
+    //Здоровье игрока
+    public int health = 10;
 
+    //Максимальное здоровье игрока
+    public int maxHealth = 10;
+
+
+    //Число собранных монет
+    public int coins;
+
+    //Префаб огненного шара и параметр Transform точки атаки
+    public GameObject fireballPrefab;
+    public Transform attackPoint;
+
+
+    //Метод, понижающий здоровье игрока
     public void TakeDamage(int damage)
     {
         health -= damage;
-        print("Р—РґРѕСЂРѕРІСЊРµ РёРіСЂРѕРєР°: " + health);
+        print("Здоровье игрока: " + health);
+    }
+
+    //Метод, увеличивающий число монеток
+    public void CollectCoins()
+    {
+        coins++;
+        print("Собранные монетки: " + coins);
+    }
+
+
+    void Update()
+    {
+
+        //Если игрок кликает левой кнопкой мыши, то создаётся огненный шар
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(fireballPrefab, attackPoint.position, attackPoint.rotation);
+        }
+
     }
 }
+
